2e98e2cd37874b70d19f04c5155ffd1c279df944
WalkingOnGreenball <walkingongreenball@gmail.com>
2023-07-17 17:26:32 +0900

    [ADD] 23/07/17 네트워크 프로그래밍 개념 및 실습
    1. TCP, IP, Port 개념 설명
    2. Socket 개념 설명 및 실습
    3. Socket을 이용한 채팅프로그램(Server, Client)구현

---
+++ src/com/kh/day16/network/exercise1/ChattingServer.java
@@ -0,0 +1,49 @@
+package com.kh.day16.network.exercise1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+// 후 실행
+public class ChattingServer {
+
+	public static void main(String[] args) {
+		String address = "127.0.0.1";
+		int port = 7777;
+		InputStream is = null;
+		OutputStream os = null;
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("연결 요청중...");
+			Socket socket = new Socket(address, port);	// 설정된 IP, PORT에 연결 요청함.
+			System.out.println("연결 성공");				// 연결된 후에는 소켓 객체 생성
+			os = socket.getOutputStream();	// 프로그램 기준 나가니까 OutputStream
+			is = socket.getInputStream();	// 프로그램 기준 들어오니까 InputStream
+			
+			while(true) {
+				// 1. 데이터 보내기
+				System.out.println("클라이언트(나) : ");
+				String message = sc.nextLine();
+				byte [] bytes = message.getBytes();	// 보낼때 버퍼에 씀 write() 메소드 사용
+				os.write(bytes);
+				os.flush();		// 버퍼 비워주기 flush()
+				// 보내기 완료
+				// 4. 데이터 받기
+				bytes = new byte[1024];
+				int readByteNo = is.read(bytes);
+				message = new String(bytes, 0, readByteNo);
+				System.out.printf("서버(상대) : %s\n", message);
+				// 받기 완료
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
